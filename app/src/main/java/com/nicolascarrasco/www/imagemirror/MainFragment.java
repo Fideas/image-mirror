ec213239aa63dc3c101e28f954d2a4c019fe3266
Nicolas Carrasco-Stevenson <niccarra@gmail.com>
2016-07-02 11:59:41 -0400

    Add basic behavior to the UI

--- app/src/main/java/com/nicolascarrasco/www/imagemirror/MainFragment.java
+++ app/src/main/java/com/nicolascarrasco/www/imagemirror/MainFragment.java
@@ -8,10 +8,14 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -23,9 +27,16 @@ public class MainFragment extends Fragment {
     @BindView(R.id.progress_bar)
     ProgressBar mProgressBar;
 
+    @BindView(R.id.button_get_image)
+    Button mActionButton;
+
+    @BindView(R.id.edit_text_image_url)
+    EditText mUrlView;
+
     public MainFragment() {
     }
 
+    // Lifecycle functions
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -42,5 +53,28 @@ public class MainFragment extends Fragment {
     public void onAttach(Context context) {
         super.onAttach(context);
         mContext = context;
+    }
+
+    // UI gesture listeners
+    @SuppressWarnings("unused")
+    @OnTextChanged(R.id.edit_text_image_url)
+    public void validateUrl(CharSequence url){
+        // Super simple validation for now
+        if (url.length() > 0){
+            mActionButton.setEnabled(true);
+        } else {
+            mActionButton.setEnabled(false);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.button_get_image)
+    public void launchGetImageService(){
+        // Change the UI to reflect the task is running
+        mProgressBar.setVisibility(View.VISIBLE);
+        mActionButton.setEnabled(false);
+        mUrlView.setEnabled(false);
+
+        // TODO: Start the service that fetches the image
     }
 }
